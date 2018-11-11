89e277ff9789c9544ab458a4cdb97c1fbe15c3f7
hanson <luoshuaihome@gmail.com>
2018-11-11 12:45:28 +0800

    add info.plist

--- XXAuthorizationKitDemo/XXAuthorizationKitDemo/XXAuthorizationModel.h
+++ XXAuthorizationKitDemo/XXAuthorizationKitDemo/XXAuthorizationModel.h
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 typedef NS_ENUM(NSInteger, AuthorizationModelType) {
     AuthorizationModelTypeLocation = 0,                         // no button type
@@ -19,5 +20,34 @@ typedef NS_ENUM(NSInteger, AuthorizationModelType) {
 @property (nonatomic, copy) NSString *VC;
 /** <#注释#> */
 @property (nonatomic, copy) NSString *type;
+/** <#注释#> */
+@property (nonatomic, copy) NSString *title;
+
+@end
+
+#pragma mark - ------------------- XXAuthorizationCell ----------------------
+
+@interface XXAuthorizationCell : UITableViewCell
+/** <#注释#> */
+@property (nonatomic, strong) XXAuthorizationModel *model;
+
+@end
+
+@implementation XXAuthorizationCell
+
+-(instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier{
+    
+    if (self = [super initWithStyle:style reuseIdentifier:reuseIdentifier]) {
+        
+        
+    }
+    return self;
+}
+
+-(void)setModel:(XXAuthorizationModel *)model {
+    
+    _model = model;
+    self.textLabel.text = model.title;
+}
 
 @end
--- XXAuthorizationKitDemo/XXAuthorizationKitDemo/XXAuthorizationViewController.m
+++ XXAuthorizationKitDemo/XXAuthorizationKitDemo/XXAuthorizationViewController.m
@@ -16,6 +16,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.title = @"XXAuthorization";
+    self.view.backgroundColor = [UIColor whiteColor];
     // Do any additional setup after loading the view.
 }
 
